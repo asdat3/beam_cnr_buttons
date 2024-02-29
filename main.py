8f50f02952771d5ce2f34bdd8bda7e5dda2a1a40
KyuDev <quentin.schilling@gmx.de>
2024-02-29 15:48:38 +0100

    added config to build

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-import pyautogui, pygetwindow, time, random, sys, os, datetime, json, zipfile, requests, ctypes, string, subprocess
-import pyscreeze
+import pyautogui, time, json, keyboard
 from pynput.keyboard import Key, Controller
 from pynput import mouse
-import keyboard
 
 mouse_controller = mouse.Controller()
 
 # RICHPRINT
 keyboard_pynput = Controller()
 
+with open("config.json","r") as f:
+    config = json.load(f)
 
 def click_some_img(path):
-    tb_1_btn = pyautogui.locateOnScreen(path,confidence=0.81)
-    if not tb_1_btn == None:
-        pyautogui.moveTo(tb_1_btn)
-        pyautogui.mouseDown()
-        time.sleep(0.1)
-        pyautogui.mouseUp()
-        print("done")
-    else:
-        print("Not found on screen please make sure UI is visible")
+    if not path == "":
+        tb_1_btn = pyautogui.locateOnScreen(path,confidence=0.81)
+        if not tb_1_btn == None:
+            pyautogui.moveTo(tb_1_btn)
+            pyautogui.mouseDown()
+            time.sleep(0.1)
+            pyautogui.mouseUp()
+            print("done")
+        else:
+            print("Not found on screen please make sure UI is visible")
 
 def on_key_press(event):
-    # print(event)
-    if "end down" in str(event): #num1 without numlock
-        print("ROB Key")
-        click_some_img('images\\rob.PNG')
-    elif "page down down" in str(event): #num3 without numlock
-        print("Repair Key")
-        click_some_img('images\\repair.PNG')
-    elif "down down" in str(event): #num2 without numlock
-        print("Respawn1 Key")
-        click_some_img('images\\respawn1.PNG')
+    if config["debug"]:
+        print(event)
+
+    if "end down" in str(event): #num1
+        print(config["num1"]["label"] + " init")
+        click_some_img(config["num1"]["path"])
+    elif "page down down" in str(event): #num3
+        print(config["num3"]["label"] + " init")
+        click_some_img(config["num3"]["path"])
+    elif "down down" in str(event): #num2
+        print(config["num3"]["label"] + " init")
+        click_some_img(config["num3"]["path"])
+    elif "left" in str(event): #num4
+        print(config["num4"]["label"] + " init")
+        click_some_img(config["num4"]["path"])
+    elif "clear" in str(event): #num5
+        print(config["num5"]["label"] + " init")
+        click_some_img(config["num5"]["path"])
+    elif "right" in str(event): #num6
+        print(config["num6"]["label"] + " init")
+        click_some_img(config["num6"]["path"])
 
 
 
